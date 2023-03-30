c1d1906fc0c879178736434d1280388e42cd9cc9
ForzenAbyss <91458357+ForzenAbyss@users.noreply.github.com>
2023-03-30 21:29:33 +0700

    Add files via upload
    Trang Login-Form

---
+++ main.js
@@ -0,0 +1,50 @@
+//================= cursor animation zoom in =============//
+
+var cursor = document.querySelector(".cursor"),
+  cursorScale = document.querySelectorAll(".cursor-scale"),
+  mouseX = 0,
+  mouseY = 0;
+
+gsap.to({}, 0.016, {
+  repeat: -1,
+
+  onRepeat: function () {
+    gsap.set(cursor, {
+      css: {
+        left: mouseX,
+        top: mouseY,
+      },
+    });
+  },
+});
+
+window.addEventListener("mousemove", function (e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+cursorScale.forEach(link => {
+  link.addEventListener("mouseleave", () => {
+    cursor.classList.remove("grow");
+    cursor.classList.remove("grow-small");
+  });
+  link.addEventListener("mousemove", () => {
+    cursor.classList.add("grow");
+    if (link.classList.contains("small")) {
+      cursor.classList.remove("grow");
+      cursor.classList.add("grow-small");
+    }
+  });
+});
+
+const toggle = document.querySelector(".toggle"),
+  input = document.querySelector("input");
+
+toggle.addEventListener("click", () => {
+  if (input.type === "password") {
+    input.type = "text";
+    toggle.classList.replace("uil-eye-slash", "uil-eye");
+  } else {
+    input.type = "password";
+  }
+});
