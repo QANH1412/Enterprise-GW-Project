a96d9751a7d60aaccd05a6ac68394b5bbf676981
QANH1412 <voquocanh1412@gmail.com>
2023-04-20 00:20:42 +0700

    Last Update
    Cannot put account-register (create) from frontend  to backend,
    So I will use default layout for account-register(create)

---
+++ WebApplication2/WebApplication2/Scripts/Fronend/AccountProfileAdmin.js
@@ -0,0 +1,65 @@
+﻿const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
+
+const handleButtonClick = e => {
+    const targetSection = e.target.getAttribute("data-section");
+    const section = document.querySelector(targetSection);
+    targetSection !== "#about"
+        ? card.classList.add("is-active")
+        : card.classList.remove("is-active");
+    card.setAttribute("data-state", targetSection);
+    sections.forEach(s => s.classList.remove("is-active"));
+    buttons.forEach(b => b.classList.remove("is-active"));
+    e.target.classList.add("is-active");
+    section.classList.add("is-active");
+};
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", handleButtonClick);
+});
+
+//========== Loader  ============//
+window.addEventListener("load", () => {
+    const loader_bg = document.querySelector(".loader_bg");
+    loader_bg.classList.add("loader_bg-finish");
+});
+
+//================= cursor animation zoom in =============//
+
+var cursor = document.querySelector(".cursor"),
+    cursorScale = document.querySelectorAll(".cursor-scale"),
+    mouseX = 0,
+    mouseY = 0;
+
+gsap.to({}, 0.016, {
+    repeat: -1,
+
+    onRepeat: function () {
+        gsap.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY,
+            },
+        });
+    },
+});
+
+window.addEventListener("mousemove", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+cursorScale.forEach(link => {
+    link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("grow");
+        cursor.classList.remove("grow-small");
+    });
+    link.addEventListener("mousemove", () => {
+        cursor.classList.add("grow");
+        if (link.classList.contains("small")) {
+            cursor.classList.remove("grow");
+            cursor.classList.add("grow-small");
+        }
+    });
+});
